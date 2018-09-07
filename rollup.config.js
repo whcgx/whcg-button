783e7e546207d34a0fee16ddd5f874406cf4fb44
Fredrik <fredrik@Fredriks-MacBook-Pro.local>
2018-09-07 17:05:27 +0200

    after build

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,42 @@
+import reslove from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+// export default [{
+//     entry: "src/index.js",
+//     dest: "dist/index.js",
+//     format: "cjs"
+//   }, {
+//     entry: "src/index.js",
+//     dest: "dist/index.es6.js",
+//     format: 'es'
+//   }]
+
+
+
+
+
+
 export default [{
-    entry: "src/index.js",
-    dest: "dist/index.js",
-    format: "cjs"
-  }, {
-    entry: "src/index.js",
-    dest: "dist/index.es6.js",
-    format: 'es'
-  }]
+    input: 'src/index.js',
+    output: {
+        file: './dist/index.es6.js',
+        format: 'es'
+    },
+    name: 'MyModulees6',
+    plugins: [
+        reslove(),
+        commonjs()
+    ]
+},
+{
+  input: 'src/index.js',
+  output: {
+      file: './dist/index.js',
+      format: 'iife'
+  },
+  name: 'MyModuleiife',
+  plugins: [
+      reslove(),
+      commonjs()
+  ]
+}];
