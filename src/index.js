e034f5ec03f81610d1ebcee003381dadb38a6e13
Fredrik <fredrik@Fredriks-MacBook-Pro.local>
2018-09-07 16:39:38 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,41 +1,37 @@
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
-import '@whcg/whcg-button/theme/whcg-button-styles.js';
 import '@vaadin/vaadin-button/src/vaadin-button.js';
- 
+import "../theme/whcg-button-styles.js";
+
 export class WhcgButton extends PolymerElement {
     static get template() {
-        return html `
-
+        return html`
 
         <style>
              vaadin-button {
-                 --whcg-host-font-size: var(--whcg-button-host-font-size);
-                 --whcg-host-color: var(--whcg-button-host-color);
-                 --whcg-host-background-color: var(--whcg-button-host-background-color);
-                 --whcg-host-border-radius: var(--whcg-button-host-border-radius);
-                 --whcg-host-border-style: var(--whcg-button-host-border-style);
-                 --whcg-host-border-width: var(--whcg-button-host-border-width);
-                 --whcg-host-border-color: var(--whcg-button-host-border-color);
-                 
-                 --whcg-host-height: var(--whcg-button-host-height);
-                 --whcg-host-min-width: var(--whcg-button-host-min-width);
-                 --whcg-host-padding-top: var(--whcg-button-host-padding-top);
-                 --whcg-host-padding-bottom: var(--whcg-button-host-padding-bottom);
-                 --whcg-host-padding-left: var(--whcg-button-host-padding-left);
-                 --whcg-host-padding-right: var(--whcg-button-host-padding-right);
-                 
-                 --whcg-label-font-size: var(--whcg-button-label-font-size);
-                 --whcg-label-font-weight: var(--whcg-button-label-font-weight);
-                 --whcg-label-font-family: var(--whcg-button-label-font-family);
+                --whcg-host-font-size: var(--whcg-button-host-font-size);
+                --whcg-host-color: var(--whcg-button-host-color);
+                --whcg-host-background-color: var(--whcg-button-host-background-color);
+                --whcg-host-border-radius: var(--whcg-button-host-border-radius);
+                --whcg-host-border-style: var(--whcg-button-host-border-style);
+                --whcg-host-border-width: var(--whcg-button-host-border-width);
+                --whcg-host-border-color: var(--whcg-button-host-border-color);
+                --whcg-host-height: var(--whcg-button-host-height);
+                --whcg-host-min-width: var(--whcg-button-host-min-width);
+                --whcg-host-padding-top: var(--whcg-button-host-padding-top);
+                --whcg-host-padding-bottom: var(--whcg-button-host-padding-bottom);
+                --whcg-host-padding-left: var(--whcg-button-host-padding-left);
+                --whcg-host-padding-right: var(--whcg-button-host-padding-right);
+                
+                --whcg-label-font-size: var(--whcg-button-label-font-size);
+                --whcg-label-font-weight: var(--whcg-button-label-font-weight);
+                --whcg-label-font-family: var(--whcg-button-label-font-family);
                 --whcg-label-line-height: var(--whcg-button-label-line-height); 
                 --whcg-label-padding-top: var(--whcg-button-label-padding-top);
                 --whcg-label-padding-bottom: var(--whcg-button-label-padding-bottom);
                 --whcg-label-padding-left: var(--whcg-button-label-padding-left);
                 --whcg-label-padding-right: var(--whcg-button-label-padding-right);
-                
+                --whcg-label-color: var(--whcg-button-label-color);
              }
-            
-
         </style>
 
         <vaadin-button>
@@ -49,42 +45,6 @@ export class WhcgButton extends PolymerElement {
 
         return {
 
-            // label: {
-            //     type: String,
-            //     notify: true,
-            //     readOnly: false,
-            // },
-            // value: {
-            //     type: String,
-            //     notify: true,
-            //     readOnly: false,
-            //     observer: '_valueChanged'
-            // },
-            // placeholder: {
-            //     type: String,
-            //     notify: true,
-            //     readOnly: false,
-            // },
-            // suffix: {
-            //     type: String,
-            //     notify: true,
-            //     readOnly: false,
-            // },
-            // kind: {
-            //     type: String,
-            //     notify: true,
-            //     readOnly: false,
-            // },
-            // period: {
-            //     type: String,
-            //     notify: true,
-            //     readOnly: false,
-            // },
-            // valueoutput: {
-            //     type: String,
-            //     notify: true,
-            //     readOnly: false,
-            // }
         }
     }
 
@@ -95,4 +55,3 @@ export class WhcgButton extends PolymerElement {
 }
 
 window.customElements.define('whcg-button', WhcgButton);
-
