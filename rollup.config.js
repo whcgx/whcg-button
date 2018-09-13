2d5842fed4863388605946b477d5ca4b1f7701c2
Andersh75 <andersh75@gmail.com>
2018-09-14 00:04:50 +0200

    add dep

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,43 @@
 // import reslove from 'rollup-plugin-node-resolve';
 // import commonjs from 'rollup-plugin-commonjs';
 
-// export default [{
-//     entry: "src/index.js",
-//     dest: "dist/index.js",
-//     format: "cjs"
-//   }, {
-//     entry: "src/index.js",
-//     dest: "dist/index.es6.js",
-//     format: 'es'
-//   }]
-
-
-
-
-
-
 export default [{
-    input: 'src/index.js',
-    output: {
-        file: './dist/index.es6.js',
-        format: 'es'
-    },
-    // external: [ 'moment' ],
-    name: 'MyModulees6'
-    // plugins: [
-    //     reslove(),
-    //     commonjs()
-    // ]
-},
-{
-  input: 'src/index.js',
-  output: {
-      file: './dist/index.js',
-      format: 'cjs'
-  },
-  name: 'MyModulecjs'
-  // plugins: [
-  //     reslove(),
-  //     commonjs()
-  //]
-}];
+    entry: "src/index.js",
+    dest: "dist/index.js",
+    format: "cjs"
+  }, {
+    entry: "src/index.js",
+    dest: "dist/index.es6.js",
+    format: 'es'
+  }]
+
+
+
+
+
+
+// export default [{
+//     input: 'src/index.js',
+//     output: {
+//         file: './dist/index.es6.js',
+//         format: 'es'
+//     },
+//     // external: [ 'moment' ],
+//     name: 'MyModulees6'
+//     // plugins: [
+//     //     reslove(),
+//     //     commonjs()
+//     // ]
+// },
+// {
+//   input: 'src/index.js',
+//   output: {
+//       file: './dist/index.js',
+//       format: 'cjs'
+//   },
+//   name: 'MyModulecjs'
+//   // plugins: [
+//   //     reslove(),
+//   //     commonjs()
+//   //]
+// }];
